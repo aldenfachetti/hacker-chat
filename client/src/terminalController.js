1eed9d19c4f9647cc1906a781045d3c8b5a552bd
Alden Fachetti <aldenmerlin@gmail.com>
2021-03-28 19:39:12 -0300

    events, initializeTable and componentEmitter

---
+++ client/src/terminalController.js
@@ -0,0 +1,10 @@
+//Responsavel por receber as regras de negocio e fazer a delegacao dos eventos
+
+export default class TerminalController {
+  constructor() {}
+
+  //comeca o projeto
+  async initializeTable(eventEmitter) {
+    console.log("Inicializou!");
+  }
+}
