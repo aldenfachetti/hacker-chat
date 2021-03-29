7403c733c139d84355d69c904902d69a281cda2a
Alden Fachetti <aldenmerlin@gmail.com>
2021-03-29 09:29:03 -0300

    chat component included

--- client/src/components.js
+++ client/src/components.js
@@ -3,25 +3,27 @@
 import blessed from "blessed";
 
 export default class ComponentBuilder {
-  #screen
-  #layout
-  #input
+  #screen;
+  #layout;
+  #input;
+  #chat;
 
   constructor() {}
 
   // propriedades comum que todos compoentes tem
-  #baseComponent() {// component privado '#'
+  #baseComponent() {
+    // component privado '#'
     return {
       border: "line",
       mouse: true,
       keys: true,
-      top: 0;
+      top: 0,
       scrollbar: {
-        ch: ' ',
-        inverse: true
+        ch: " ",
+        inverse: true,
       },
       // habilita colocar cores e tags no texto
-      tags: true
+      tags: true,
     };
   }
 
@@ -29,24 +31,24 @@ export default class ComponentBuilder {
     this.#screen = blessed.screen({
       // faz alguns redimensionamentos automaticos na tela
       smartCSR: true,
-      title
-    })
+      title,
+    });
 
     // para o programa
-    this.#screen.key(['escape', 'q', 'C-c'], () => process.exit(0));
+    this.#screen.key(["escape", "q", "C-c"], () => process.exit(0));
 
-    return this
+    return this;
   }
 
   // cria os quadrados primeiro
   setLayoutComponent() {
     this.#layout = blessed.layout({
       parent: this.screen,
-      width: '100%',
-      height: '100%'
-    })
+      width: "100%",
+      height: "100%",
+    });
 
-    return this
+    return this;
   }
 
   // pra pegar o text area, pegar nosso texto no terminal
@@ -54,21 +56,45 @@ export default class ComponentBuilder {
     const input = blessed.textarea({
       parent: this.#screen,
       bottom: 0,
-      height: '10%',
+      height: "10%",
       inputOnFocus: true,
       padding: {
         top: 1,
-        left: 2
+        left: 2,
       },
       style: {
-        fg: '#f6f6f6',
-        bg: '#353535'
-      }
-    })
+        fg: "#f6f6f6",
+        bg: "#353535",
+      },
+    });
 
-    input.key('enter', onEnterPressed)
-    this.#input = input
+    input.key("enter", onEnterPressed);
+    this.#input = input;
 
-    return this
+    return this;
+  }
+
+  setChatComponent() {
+    this.#chat = blessed.list({
+      ...this.#baseComponent(),
+      parent: this.#layout,
+      align: "left",
+      width: "50%",
+      height: "90%",
+      items: ["{bold}Menssenger{/}"],
+    });
+
+    return this;
+  }
+
+  // responsavel por entregar uma factory, nosso objeto que precisamos para a tela
+  build() {
+    const components = {
+      // deixando publico somente essas variaveis
+      screen: this.#screen,
+      input: this.#input,
+    };
+
+    return components;
   }
 }
--- client/src/terminalController.js
+++ client/src/terminalController.js
@@ -1,10 +1,30 @@
 //Responsavel por receber as regras de negocio e fazer a delegacao dos eventos
+// Controller eh o nosso mediador, ele que vai inicializar todo mundo
+
+import ComponentsBuilder from "./components.js";
 
 export default class TerminalController {
   constructor() {}
 
+  #onInputReceived(eventEmitter) {
+    return function () {
+      const message = this.getValue();
+      console.log(message);
+      this.clearValue();
+    };
+  }
+
   //comeca o projeto
   async initializeTable(eventEmitter) {
-    console.log("Inicializou!");
+    // fazendo nosso builder entrar em acao
+    const components = new ComponentsBuilder()
+      .setScreen({ title: "Hacker Chat - Alden Merlin" })
+      .setLayoutComponent()
+      .setInputComponent(this.#onInputReceived(eventEmitter))
+      .setChatComponent()
+      .build();
+
+    components.input.focus();
+    components.screen.render();
   }
 }
